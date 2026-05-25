e078c75fbc62f083a45f570f5e3bbf73bd73bc64
TudorBogos <tudor.bogos.tb@gmail.com>
2026-05-25 05:07:45 +0300

    fix(migrations): correct typo in password for tb171 in user mappings

--- db/migrations/001_create_maas_user_mappings.sql
+++ db/migrations/001_create_maas_user_mappings.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS maas_user_mappings (
 INSERT INTO maas_user_mappings (username, maas_password)
 VALUES
     ('student1', 'parola1'),
-    ('tb171', 'parol1234')
+    ('tb171', 'parola1234')
 ON CONFLICT(username) DO UPDATE SET
     maas_password = excluded.maas_password;
