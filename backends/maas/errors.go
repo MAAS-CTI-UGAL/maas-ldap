8a4d53a51baa9318bf8f4957903d2b0e0961d2c7
TudorBogos <tudor.bogos.tb@gmail.com>
2026-06-10 12:53:56 +0300

    refactor(backends): add scoped error writer

--- backends/maas/errors.go
+++ backends/maas/errors.go
@@ -1,18 +1,7 @@
 package maas
 
-import (
-	"log"
-	"net/http"
-)
+import "maas-ldap/backends/errorwriter"
 
 // WriteError logs a MAAS operation failure and writes the public HTTP error response.
 // logMessage is only logged; userMessage is sent to the client.
-func WriteError(w http.ResponseWriter, operation string, logMessage string, userMessage string, internalErr error, statusCode int) {
-	if internalErr != nil {
-		log.Printf("%s failed: %s: %v", operation, logMessage, internalErr)
-	} else {
-		log.Printf("%s failed: %s", operation, logMessage)
-	}
-
-	http.Error(w, userMessage, statusCode)
-}
+var WriteError = errorwriter.New("maas")
